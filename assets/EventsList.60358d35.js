9ea1fffdf7addb580a249a3afc9ca6beae84489e
Straviux <arvin.heredero@gmail.com>
2022-11-28 11:13:09 +0800

    minor layout update

--- assets/EventsList.60358d35.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index.46cff267.js";const e={};function n(r,c){return null}const _=t(e,[["render",n]]);export{_ as default};
---
+++ assets/EventsView.250dd455.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.12b168ea.js";const n={};function r(t,c){return null}const s=e(n,[["render",r]]);export{s as default};
