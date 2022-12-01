078e10041f144d1c03c14ac908dd493215ac07f1
Straviux <arvin.heredero@gmail.com>
2022-12-01 12:44:02 +0800

    new build

---
+++ assets/EventsList.7cb13351.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index.a5abb04d.js";const e={};function n(r,c){return null}const _=t(e,[["render",n]]);export{_ as default};
--- assets/EventsView.5731f214.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.31ea2d9f.js";const n={};function r(t,c){return null}const s=e(n,[["render",r]]);export{s as default};
